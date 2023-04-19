9a56e127af40796028048bf078b9ee273c727014
daviguindani <davigpvieira@gmail.com>
2023-04-19 20:25:59 -0300

    changing the names to english and using auto formater

--- BotTelegram.py
+++ BotTelegram.py
@@ -1,44 +1,45 @@
 import telebot
+import os
 from datetime import datetime as dt
 
-key = "" #here you can type your token api
-bot = telebot.TeleBot(key)
+bot = telebot.TeleBot(os.environ.get("TOKEN"))
 
-@bot.message_handler(commands = ["Abrir"])
+@bot.message_handler(commands=["Abrir"])
 def responder(mensagem):
     bot.send_message(mensagem.chat.id, "Vou verificar se você tem a senha")
 
-    with open("BD.txt", "r") as arquivo:
-        teste = arquivo.readlines()
+    with open("BD.txt", "r") as dataBaseTXT:
+        dataBase = dataBaseTXT.readlines()
 
-    aut = False
-    for linha in teste:
-        linha = linha[:-1]
-        if str(mensagem.from_user.id) == linha:
-            aut = True
+    autorized = False
+    for line in dataBase:
+        if str(mensagem.from_user.id) == line[:-1]:
+            autorized = True
 
-    if aut:
-        data = dt.now()
-        #here is recommended to manipulate the "data" using the day, month or something like that
-
-        bot.send_message(mensagem.chat.id, data)
+    if autorized:
+        password = dt.now()
+        bot.send_message(mensagem.chat.id, password)
     else:
         bot.send_message(mensagem.chat.id, "Nao abre")
 
-@bot.message_handler(commands = ["Mandar"])
+
+@bot.message_handler(commands=["Mandar"])
 def responder(mensagem):
     bot.send_message(mensagem.chat.id, "Valeu! O SharKey manda outro <3")
+
 
 def verificar(mensagem):
     return True
 
-@bot.message_handler(func = verificar)
+
+@bot.message_handler(func=verificar)
 def responder(mensagem):
-    texto = """
+    text = """
     Olá, eu sou o SharKey, Bot da Tranca da Focus, clique em um item
      /Abrir a porta
      /Mandar o abraço pro SharKey
     """
-    bot.send_message(mensagem.chat.id, texto)
+    bot.send_message(mensagem.chat.id, text)
 
-bot.polling ()
+
+bot.polling()
